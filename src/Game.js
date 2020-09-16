51a127e35f41c8e34b8c4a2ef24e5b4c51c25eb1
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-09-16 09:49:48 -0700

    Add time travel feature

--- src/Game.js
+++ src/Game.js
@@ -24,32 +24,87 @@ function Square(props) {
  * each square object.
  */
 class Board extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            winner: null,
-            lastMove: 'O',
-            squares: Array(16).fill(null),
-            squareStyle: Array(16).fill('square'),
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
+
     /**
+     * Return a Square component.
+     * Props defined in Board component
+     * are passed down to the Square
+     * component.
      *
+     * A special prop 'onClick' holds a
+     * closure that will be assigned as 
+     * the onClick for the square that will
+     * trigger the handleClick() method
+     * from the Board component.
      */
     renderSquare(i) {
         return <Square 
-            value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}
-            boxStyle={this.state.squareStyle[i]}
+            value={this.props.squares[i]} 
+            onClick={() => this.props.SquareButtonClickHandler(i)}
+            boxStyle={this.props.squareStyle[i]}
         />;
+    }
+
+    /**
+     * Render the game board
+     */
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
+                    {this.renderSquare(15)}
+                </div>
+            </div>
+        );
+    }
+}
+
+/**
+ * Component for the Game
+ * Shows the actual game
+ * and game info
+ */
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            winner: null,
+            lastMove: 'O',
+            history: [],
+            squares: Array(16).fill(null),
+            squareStyle: Array(16).fill('square'),
+        };
+        this.handleBoardAction = this.handleBoardAction.bind(this);
+        this.handleTimeTravel = this.handleTimeTravel.bind(this);
+        this.handleSquareClick = this.handleSquareClick.bind(this);
     }
 
     /**
      * Click handler for square button.
      *
      */
-    handleClick(i) {
+    handleSquareClick(i) {
         if (this.state.squares[i] !== null 
          || this.state.winner !== null) {
             return false;
@@ -69,17 +124,20 @@ class Board extends React.Component {
         }
 
         let winner = this.checkWinner(state);
+        let boardUpdate  = {
+            value: mySymbol,
+            position: i,
+        };
         if (winner !== null) {
             state.winner = winner.winner;
             state.coordinates = winner.coordinates;
             winner.coordinates.forEach((value) => {
                 state.squareStyle[value] = 'square-winner';
             });
+            boardUpdate.winningCoordinates = winner.coordinates.slice();
         }
-
+        this.handleBoardAction(boardUpdate);
         this.setState(state);
-
-
     }
 
     /**
@@ -180,55 +238,80 @@ class Board extends React.Component {
         }
     }
 
+    renderHistoryItems(item) {
+        return <HistoryItem
+            key={item.moveId.toString()}
+            entry={item}
+            timeTravelHandler={() => this.handleTimeTravel(item)}
+        />
+    }
+
+    handleTimeTravel(replayUpTo) {
+        this.setState((state) => {
+            let x = 0;
+            let newSquares = Array(16).fill(null);
+            let newSquareStyle = Array(16).fill('square');
+            while (x <= replayUpTo.moveId) {
+                newSquares[state.history[x].add.position] = state.history[x].add.symbol
+                if (state.history[x].winningCoordinates) {
+                    state.history[x].winningCoordinates.forEach((position) => {
+                        newSquareStyle[position] = 'square-winner';
+                    });
+                }
+                x++;
+            }
+            return {
+                squares: newSquares,
+                squareStyle: newSquareStyle
+            }
+        });
+    }
+
+    handleBoardAction(action) {
+        this.setState((state) => {
+            return {
+                history: state.history.concat({
+                    value: action.value + ' on position ' + action.position,
+                    add: {
+                        symbol: action.value,
+                        position: action.position
+                    },
+                    moveId: state.history.length,
+                    winningCoordinates: action.winningCoordinates
+                })
+            }
+        })
+    }
 
     /**
-     *
+     * Render the game and any game-info
      */
     render() {
+        let items = this.state.history.map((item) => {
+            return this.renderHistoryItems(item);
+        });
         let status = '';
         if (this.state.winner !== null) {
             status = 'Winner: ' + this.state.winner;
-
-            /*
-            let squareStyle = [];
-            winner.coordinates.forEach((value) => {
-                squareStyle[value] = 'square-selected';
-            });
-            this.setState({
-                squareStyle: squareStyle
-            });
-            */
         }
         else {
             let player = (this.state.lastMove === 'O') ?'X' : 'O';
             status = 'Next player: ' + player.toString();
         }
         return (
-            <div>
+            <div className="game">
+                <div className="game-board">
                 <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                    {this.renderSquare(3)}
+                    <Board 
+                        BoardActionHandler={this.handleBoardAction}
+                        SquareButtonClickHandler={this.handleSquareClick}
+                        squares={this.state.squares}
+                        squareStyle={this.state.squareStyle}
+                    />
                 </div>
-                <div className="board-row">
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(8)}
-                    {this.renderSquare(9)}
-                    {this.renderSquare(10)}
-                    {this.renderSquare(11)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(12)}
-                    {this.renderSquare(13)}
-                    {this.renderSquare(14)}
-                    {this.renderSquare(15)}
+                <div className="game-info">
+                    <div>Moves</div>
+                    <ol>{items}</ol>
                 </div>
             </div>
         );
@@ -236,27 +319,16 @@ class Board extends React.Component {
 }
 
 /**
- * Component for the Game
- * Shows the actual game
- * and game info
+ * Function component to render
+ * history items for game activity
  */
-class Game extends React.Component {
-    /**
-     *
-     */
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/**/}</div>
-                    <ol>{/**/}</ol>
-                </div>
-            </div>
-        );
-    }
+function HistoryItem(props) {
+    return (
+        <li>
+            <button
+                onClick={props.timeTravelHandler}
+            >{props.entry.value}</button>
+        </li>
+    );
 }
-
 export default Game;
